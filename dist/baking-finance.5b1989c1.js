f933dc87ee4889801111ae13064e1daecce9c354
daniel_nomadic <daniel.jean@nomadic-labs.com>
2021-09-27 16:24:23 +0200

    fix color

--- dist/baking-finance.5b1989c1.js
+++ dist/baking-finance.5b1989c1.js
@@ -169,7 +169,7 @@ module.exports = reloadCSS;
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./images/logo.png":91,"_css_loader":47}],138:[function(require,module,exports) {
+},{"./images/logo.png":91,"_css_loader":47}],86:[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -198,7 +198,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '39825' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '42075' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -418,4 +418,4 @@ LazyPromise.prototype.catch = function (onError) {
 };
 },{"./bundle-url":49}],0:[function(require,module,exports) {
 var b=require(140);b.load([["logo.98ad69f8.png",91]]);
-},{}]},{},[138,0], null)
+},{}]},{},[86,0], null)
